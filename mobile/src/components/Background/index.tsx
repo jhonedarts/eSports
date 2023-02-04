fbd1f61dea7e49729a44da4b85f9acd85ba936d5
Jhone Darts <jhonedarts@gmail.com>
2023-02-04 12:01:14 -0300

    ✨ creted homepage and gameCard list

---
+++ mobile/src/components/Background/index.tsx
@@ -0,0 +1,18 @@
+import { ImageBackground } from 'react-native';
+import { styles } from './styles';
+import backgroungImg from '../../assets/background-galaxy.png';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export function Background({ children }:Props) {
+  return (
+    <ImageBackground
+      source={backgroungImg}
+      defaultSource={backgroungImg}
+      style={styles.container}>
+      {children}
+    </ImageBackground>
+  );
+}
